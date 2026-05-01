ddc8242cf2372c80028237f5c2e730fc9ed0ae61
Amira Abdelkarim <amiraabdelkariim10@gmail.com>
2026-05-01 15:31:08 +0300

    BudgetManager  class

---
+++ BudgetManager.java
@@ -0,0 +1,79 @@
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BudgetManager {
+
+    // FR-2
+    public double calculateInitialDailyLimit(BudgetCycle cycle) {
+
+        long days = ChronoUnit.DAYS.between(
+                cycle.getStartDate(),
+                cycle.getEndDate()
+        ) + 1;
+
+        if (days <= 0) return 0;
+
+        return cycle.getTotalAllowance() / days;
+    }
+
+    // total spent
+    public double calculateTotalSpent(List<Transaction> transactions) {
+
+        double total = 0;
+
+        for (Transaction t : transactions) {
+            total += t.getAmount();
+        }
+
+        return total;
+    }
+
+    // FR-4 / US3
+    public int getRemainingDays(BudgetCycle cycle) {
+
+        long days = ChronoUnit.DAYS.between(
+                LocalDate.now(),
+                cycle.getEndDate()
+        ) + 1;
+
+        return (int) Math.max(days, 0);
+    }
+
+    // FR-4 (Dynamic Safe Daily Limit)
+    public double calculateDailyLimit(BudgetCycle cycle, List<Transaction> transactions) {
+
+        double spent = calculateTotalSpent(transactions);
+        double remaining = cycle.getTotalAllowance() - spent;
+        int days = getRemainingDays(cycle);
+
+        if (days <= 0) return 0;
+
+        return remaining / days;
+    }
+
+    // FR-6
+    public boolean isEightyPercentReached(BudgetCycle cycle, List<Transaction> transactions) {
+
+        return calculateTotalSpent(transactions)
+                >= 0.8 * cycle.getTotalAllowance();
+    }
+
+    // FR-5
+    public Map<String, Double> categoryTotals(List<Transaction> transactions) {
+
+        Map<String, Double> map = new HashMap<>();
+
+        for (Transaction t : transactions) {
+
+            map.put(
+                    t.getCategory(),
+                    map.getOrDefault(t.getCategory(), 0.0) + t.getAmount()
+            );
+        }
+
+        return map;
+    }
+}
